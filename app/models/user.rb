333de994faee5d4df5b9754fafa331ba712fe213
Flavio Bordoni <flavio.bordoni.dev@gmail.com>
2020-08-07 15:28:49 +0000

    Implement home3

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,4 +44,11 @@ class User < ApplicationRecord
 
   # == Instance Methods =====================================================
 
+  def name_shown
+    if self.present? == true
+      return self.name.blank? ? self.email.split('@')[0].capitalize : self.name
+    else
+      return "nessun utente loggato"
+    end
+  end
 end
