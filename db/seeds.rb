621b104adcdb69a5e116a2cfef291f3256b84ffb
Flavio Bordoni <flavio.bordoni.dev@gmail.com>
2020-08-24 09:19:14 +0000

    add seed companies

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,19 +13,24 @@
 #  load seed
 #end
 
-puts "Inseriamo tre articoli per tre utenti"
 
-u1 = User.find(1)
-u1.eg_posts.create(headline: "Il mio primo articolo", incipit: "Perché scrivere questo articolo")
-u1.eg_posts.create(headline: "Il mio secondo articolo", incipit: "Ci ho preso gusto")
-u1.eg_posts.create(headline: "Il mio terzo articolo", incipit: "Adesso sono esperto")
+# puts "Inseriamo tre articoli per tre utenti"
 
-u2 = User.find(2)
-u2.eg_posts.create(headline: "La conferenza uno", incipit: "Una interessante conferenza sul cielo")
-u2.eg_posts.create(headline: "La conferenza due", incipit: "Perché si formano le nuvole? Capiamo il ciclo dell'acqua")
-u2.eg_posts.create(headline: "La conferenza tre", incipit: "Il sole è una stella nana")
+# u1 = User.find(1)
+# u1.eg_posts.create(headline: "Il mio primo articolo", incipit: "Perché scrivere questo articolo")
+# u1.eg_posts.create(headline: "Il mio secondo articolo", incipit: "Ci ho preso gusto")
+# u1.eg_posts.create(headline: "Il mio terzo articolo", incipit: "Adesso sono esperto")
 
-u3 = User.find(3)
-u3.eg_posts.create(headline: "Studio di caso alfa", incipit: "In questo studio la nostra azienda è stata brava")
-u3.eg_posts.create(headline: "Studio di caso beta", incipit: "In questo studio identifichiamo i pesci nell'acquario")
-u3.eg_posts.create(headline: "Studio di caso gamma", incipit: "Questo studio è praticamente identico a quello dell'architetto")
+# u2 = User.find(2)
+# u2.eg_posts.create(headline: "La conferenza uno", incipit: "Una interessante conferenza sul cielo")
+# u2.eg_posts.create(headline: "La conferenza due", incipit: "Perché si formano le nuvole? Capiamo il ciclo dell'acqua")
+# u2.eg_posts.create(headline: "La conferenza tre", incipit: "Il sole è una stella nana")
+
+# u3 = User.find(3)
+# u3.eg_posts.create(headline: "Studio di caso alfa", incipit: "In questo studio la nostra azienda è stata brava")
+# u3.eg_posts.create(headline: "Studio di caso beta", incipit: "In questo studio identifichiamo i pesci nell'acquario")
+# u3.eg_posts.create(headline: "Studio di caso gamma", incipit: "Questo studio è praticamente identico a quello dell'architetto")
+
+puts "setting the Company data with I18n :en :it"
+Company.new(name: "ABC", sector: "Chemical", locale: :en).save
+Company.last.update(sector: "Chimico", locale: :it)
